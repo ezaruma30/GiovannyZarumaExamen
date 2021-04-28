018c2759eaad9a1d5878b1f3a82d3ab057027a58
Giovanny Zaruma <geos_20@yahoo.com>
2021-04-27 23:40:43 -0500

    Add project files.

---
+++ GiovannyZarumaExamen/Login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GiovannyZarumaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Boton Ingreso de Login
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUser.Text;
+            string clave = txtPass.Text;
+
+            if ((usuario == "estudiante2021") && (clave == "uisrael2021"))
+            {
+                await Navigation.PushAsync(new Registro(usuario, clave));
+            }
+            else
+            {
+                lblResultado.Text = "El usuario o contraseña es incorrecto";
+            }
+        }
+    }
+}
---
+++ GiovannyZarumaExamen/Registro.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GiovannyZarumaExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario, string clave)
+        {
+            InitializeComponent();
+            lblUser.Text = usuario;
+        }
+        /// <summary>
+        /// Boton de Calcular
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnTotal_Clicked(object sender, EventArgs e)
+        {
+            double montototal = 1800;
+            double primeraporte = Convert.ToDouble(txtMontoInicial.Text);
+
+            double valordiferencia = montototal - primeraporte; // valor de la diferencia de 1800
+
+            double primerdiferido = (valordiferencia / 3);// valor sin el 5%
+
+            double valor5 = (montototal * 0.05) / 3; // porcentaje del 5% dividido para 3 meses
+
+            txtPagoMensual.Text = Convert.ToString(primerdiferido + valor5);
+  
+
+        }
+
+        /// <summary>
+        /// Boton de Guardar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPagoMensual.Text))
+            {
+                DisplayAlert("Alerta!!", "Favor realizar el calculo", "Salir");
+            }
+            else 
+            {
+                
+                DisplayAlert("Datos Guardados!!", "Elemento guardado con exito", "Salir");
+                string usuario = lblUser.Text;
+                string nombreEstudiante = txtNombre.Text;
+                double primerAbono = Convert.ToDouble(txtMontoInicial.Text);
+                double pagomensual = Convert.ToDouble(txtPagoMensual.Text);
+                await Navigation.PushAsync(new Resumen(usuario,nombreEstudiante, primerAbono, pagomensual));
+            }
+        }
+    }
+}
